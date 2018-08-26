fbf090827afb4803f55e04959ec7bb07f97ce2a9
Jeremy Cutler <jeremy.cutler@hulu.com>
2018-08-26 10:10:57 -0700

    debug logging and a sleep for the web get

--- device.py
+++ device.py
@@ -6,6 +6,7 @@ import requests
 from threading import Thread
 import time
 
+DEBUG = True
 
 STATE_END = 0
 STATE_SONG = 1
@@ -17,6 +18,11 @@ STATE_WHITENOISE_LVL2 = 5
 SONG_END = pygame.USEREVENT + 1
 
 
+def log_debug(msg):
+    if DEBUG:
+        print(msg)
+
+
 class ChangeWorker(Thread):
 
     FIVE_MINUTES = 60 * 60 * 24 * 5
@@ -24,6 +30,7 @@ class ChangeWorker(Thread):
     end_processing = False
 
     def __init__(self, url, username, password, change_queue):
+        log_debug("Init ChangeWorker")
         Thread.__init__(self)
         self.username = username
         self.password = password
@@ -32,6 +39,7 @@ class ChangeWorker(Thread):
 
     def get_msg(self):
         response = requests.get(self.url, auth=(self.username, self.password))
+        log_debug("Request: %d, %s" % (response.status_code, response.text))
         if response.status_code == 200:
             return response.json()
         else:
@@ -43,9 +51,15 @@ class ChangeWorker(Thread):
 
             if event and event['create_date'] < time.time() - self.FIVE_MINUTES:
                 if event['mode'] == STATE_WHITENOISE and event['level'] == 2:
-                    self.change_queue.put(STATE_WHITENOISE_LVL2)
+                    mode = STATE_WHITENOISE_LVL2
                 else:
-                    self.change_queue.put(event['mode'])
+                    mode = event['mode']
+
+                log_debug("Queueing command: %s" % mode)
+
+                self.change_queue.put()
+
+            time.sleep(1)
 
     def stop(self):
         self.end_processing = True
@@ -67,6 +81,8 @@ class NurseryClient(object):
     song_end_cb = None
 
     def __init__(self):
+        log_debug("Init NurseryClient")
+
         parser = ConfigParser()
         found = parser.read(self.CONFIG_FILENAME)
 
@@ -86,10 +102,12 @@ class NurseryClient(object):
         self.change_queue = SimpleQueue()
 
     def play_song(self):
+        log_debug("Playing song")
         pygame.mixer.music.load(self.song_file)
-        pygame.mixer.music.play(0)
+        pygame.mixer.music.play(loops=0, start=378) # remove this start change
 
     def play_whitenoise(self, level_two=False):
+        log_debug("Playing whitenoise. Level 2? %s" % level_two)
         if level_two:
             pygame.mixer.music.load(self.WHITENOISE_LVL2_FILE)
             pygame.mixer.music.play(loops=0, start=self.LEVEL2_FULL_SECS-self.LEVEL2_PLAY_SECS)
@@ -98,38 +116,45 @@ class NurseryClient(object):
             pygame.mixer.music.play(loops=-1)
 
     def go_whitenoise(self):
+        log_debug("Go Whitenoise")
         self.song_end_cb = None
         self.state = STATE_WHITENOISE
         self.play_whitenoise()
 
     def go_whitenoise_lvl2(self):
+        log_debug("Go Whitenoise Lvl2")
         self.song_end_cb = self.go_whitenoise
         self.state = STATE_WHITENOISE_LVL2
         self.play_whitenoise(level_two=True)
 
     def go_song_loop(self, start_song=False):
+        log_debug("Go Song Loop. Start? %s" % start_song)
         self.song_end_cb = self.play_song
         self.state = STATE_SONG_LOOP
         if start_song:
             self.play_song()
 
     def go_song(self, start_song=False):
+        log_debug("Go Song. Start? %s" % start_song)
         self.song_end_cb = None
         self.state = STATE_SONG
         if start_song:
             self.play_song()
 
     def go_song_then_whitenoise(self, start_song=False):
+        log_debug("Go Song Then Whitenoise. Start? %s" % start_song)
         self.song_end_cb = self.go_whitenoise
         self.state = STATE_SONG_THEN_WHITENOISE
         if start_song:
             self.play_song()
 
     def go_end(self):
+        log_debug("Go End")
         self.song_end_cb = None
         self.state = SONG_END
 
     def fadeout_then_do(self, func, arg):
+        log_debug("Fadeout, then... %s(%s)" % (func, arg))
         pygame.mixer.music.fadeout(self.FADEOUT_MSECS)
         while not any([event.type == SONG_END for event in pygame.event.get()]):
             pass
@@ -145,6 +170,8 @@ class NurseryClient(object):
             return None
 
     def handle_event(self, event):
+        log_debug("Handling event: %s -> %s" % (self.state, event))
+
         if event == SONG_END and self.song_end_cb:
             self.song_end_cb(self)
 
@@ -190,6 +217,7 @@ class NurseryClient(object):
     def run(self):
         self.start_worker()
         while True:
+            print('.', end='')
             event = self.get_event()
             if event:
                 self.handle_event(event)
@@ -199,6 +227,7 @@ class NurseryClient(object):
     @staticmethod
     def startup():
         try:
+            log_debug("Client startup")
             client = NurseryClient()
             client.run()
         except Exception as e:
