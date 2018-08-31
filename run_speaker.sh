154445a770bfcf886e4157707a0f1afe313086b4
Jeremy Cutler <cutler.jeremy@gmail.com>
2018-08-31 16:55:45 +0000

    update run script to output what happens

--- run_speaker.sh
+++ run_speaker.sh
@@ -5,5 +5,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 RUNCOMMAND="${DIR}/env/bin/python ${DIR}/device.py"
 
 if [[ -z $(pidof $RUNCOMMAND) ]]; then
+    echo "Process not found. Running command."
     $RUNCOMMAND &
+else
+    echo "Process found."
 fi
