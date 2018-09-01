30917c9914d9ce67d0c19bc35544b5e5161300da
Jeremy Cutler <cutler.jeremy@gmail.com>
2018-09-01 21:35:26 +0000

    prevent restart when stopfile present

--- run_speaker.sh
+++ run_speaker.sh
@@ -4,9 +4,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 RUNCOMMAND="${DIR}/env/bin/python ${DIR}/device.py"
 
+STOPFILE="/tmp/nursery_speaker_stop_file"
+
+
 if [[ -z $(pidof $RUNCOMMAND) ]]; then
-    echo "Process not found. Running command."
-    $RUNCOMMAND &
+    if [[ -e $STOPFILE ]]; then
+        echo "Process not found. Stopfile present. Exiting."
+	exit
+    else
+        echo "Process not found. Running command."
+        $RUNCOMMAND &
+    fi
 else
     echo "Process found."
 fi
